ed2c258dee223fba6e7e5ab66770c2b3984f9b1f
Felipe Marinho <felipmarinho@gmail.com>
2020-05-01 05:51:12 -0300

    Relationship enter models

--- src/database/migrations/1588319941877-CreateUsers.ts
+++ src/database/migrations/1588319941877-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1588319941877 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
