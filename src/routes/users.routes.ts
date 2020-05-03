560de5cae07ef03868e46b1f17465f51b6d834d5
Felipe Marinho <felipmarinho@gmail.com>
2020-05-02 21:39:31 -0300

    Handling with errors

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,21 +11,17 @@ const userRouter = Router();
 const upload = multer(uploadConfig);
 
 userRouter.post('/', async (request, response) => {
-  try {
-    const { name, email, password } = request.body;
-    const createUser = new CreateUserService();
-    const user = await createUser.execute({
-      name,
-      email,
-      password,
-    });
+  const { name, email, password } = request.body;
+  const createUser = new CreateUserService();
+  const user = await createUser.execute({
+    name,
+    email,
+    password,
+  });
 
-    delete user.password;
+  delete user.password;
 
-    return response.send(user);
-  } catch (error) {
-    return response.status(400).json({ error: error.message });
-  }
+  return response.send(user);
 });
 
 userRouter.patch(
@@ -33,19 +29,15 @@ userRouter.patch(
   ensureAuthenticated,
   upload.single('avatar'),
   async (request, response) => {
-    try {
-      const updateUserAvatarService = new UpdateUserAvatarService();
-      const user = await updateUserAvatarService.execute({
-        user_id: request.user.id,
-        avatarFileName: request.file.filename,
-      });
+    const updateUserAvatarService = new UpdateUserAvatarService();
+    const user = await updateUserAvatarService.execute({
+      user_id: request.user.id,
+      avatarFileName: request.file.filename,
+    });
 
-      delete user.password;
+    delete user.password;
 
-      return response.json(user);
-    } catch (error) {
-      return response.status(400).json({ error: error.message });
-    }
+    return response.json(user);
   },
 );
 
