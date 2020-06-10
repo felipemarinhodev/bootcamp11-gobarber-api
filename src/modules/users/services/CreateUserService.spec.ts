3af5d9c8927c2bf76dfdd85756acacf02c9d816a
Felipe Marinho <felipmarinho@gmail.com>
2020-06-09 21:09:32 -0300

    Testando autenticação

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,11 +1,16 @@
 import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import CreateUserService from './CreateUserService';
 
 describe('CreateAppointment', () => {
   it('should be able to create a new user', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
     const user = await createUser.execute({
       name: 'John Doe',
       email: 'johndoe@email.com',
@@ -17,7 +22,11 @@ describe('CreateAppointment', () => {
 
   it('should not be able to create a new user with dame email from another', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
-    const createUser = new CreateUserService(fakeUsersRepository);
+    const fakeHashProvider = new FakeHashProvider();
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
     await createUser.execute({
       name: 'John Doe',
       email: 'johndoe@email.com',
