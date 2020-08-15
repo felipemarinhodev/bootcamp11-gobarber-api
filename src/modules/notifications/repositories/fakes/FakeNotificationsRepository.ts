a14662471fd6349ee5d0af7d4b3c82074b492429
Felipe Marinho <felipmarinho@gmail.com>
2020-08-15 11:02:12 -0300

    Refatorando testes

---
+++ src/modules/notifications/repositories/fakes/FakeNotificationsRepository.ts
@@ -0,0 +1,22 @@
+import INotificationsRepository from '@modules/notifications/repositories/INotificationRepository';
+import { ObjectId } from 'mongodb';
+import ICreateNotificationDTO from '@modules/notifications/dtos/ICreateNotificationDTO';
+
+import Notification from '../../infra/typeorm/schemas/Notification';
+
+class NotificationsRepository implements INotificationsRepository {
+  private notifications: Notification[] = [];
+
+  public async create({
+    recipient_id,
+    content,
+  }: ICreateNotificationDTO): Promise<Notification> {
+    const notification = new Notification();
+
+    Object.assign(notification, { id: new ObjectId(), content, recipient_id });
+    this.notifications.push(notification);
+
+    return notification;
+  }
+}
+export default NotificationsRepository;
